5932556e8ad6773db5880b4737004a74e14b05d2
Dihni Hassan <dihnihassan@gmail.com>
2023-01-23 21:55:10 -0500

    Complete exercises 1 - 4

--- exercises/exercise_2.rb
+++ exercises/exercise_2.rb
@@ -5,3 +5,9 @@ puts "Exercise 2"
 puts "----------"
 
 # Your code goes here ...
+
+
+@store1 = Store.find(1)
+@store1.update(name: "Vancouver")
+
+@store2 = Store.find(2)
--- exercises/exercise_4.rb
+++ exercises/exercise_4.rb
@@ -7,3 +7,23 @@ puts "Exercise 4"
 puts "----------"
 
 # Your code goes here ...
+Store.create(name: "Surrey", annual_revenue: 224000, mens_apparel: false, womens_apparel: true)
+
+Store.create(name: "Whistler", annual_revenue: 1900000, mens_apparel: true, womens_apparel: false)
+
+Store.create(name: "Yaletown", annual_revenue: 430000, mens_apparel: true, womens_apparel: true)
+
+# @mens_stores = Store.where("mens_apparel = 'true'")
+@mens_stores = Store.where(mens_apparel: true)
+
+
+
+@mens_stores.each do |store|
+
+  store_name = store.name
+  annual_revenue = store.annual_revenue
+  puts "#{store.name} #{store.annual_revenue}"
+  # puts store.annual_revenue
+end
+
+# puts @mens_stores
